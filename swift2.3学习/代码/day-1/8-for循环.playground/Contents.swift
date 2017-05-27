af8cb3dbf182946411dd47e6ce3cb269551ccb20
gouzi <gouzi@wangjun.local>
2017-05-27 16:17:20 +0800

    添加for循环

--- swift2.3学习/代码/day-1/8-for循环.playground/Contents.swift
+++ swift2.3学习/代码/day-1/8-for循环.playground/Contents.swift
@@ -2,3 +2,24 @@
 
 import UIKit
 
+/// 最主要的是for循环
+/// 打印0..<10
+/// for后面的括号是可以省略的
+//for var i = 0; i < 10; i += 1 {
+//    print(i)
+//}
+
+/// for in
+for j in 0..<10 {
+    print(j)
+}
+
+/// for循环的forin写法
+//for k in 0...9 {
+//    print("hello")
+//}
+
+/// 在swift中一个标志符不需要使用，可以用 `_`来代替
+for _ in 0...9 {
+    print("hha")
+}
