d0dbfe9bf5fce72290be906e2570c3f9545f3553
gouzi <gouzi@wangjun.local>
2017-05-27 10:25:47 +0800

    基本运算

---
+++ swift2.3学习/代码/day-1/4-swift中的基本运算.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// swift中没有隐式转换，所有的数据的运算，都需要相同的数据类型
+let n = 2.3
+let m = 10
+// 将整型转为浮点型 Double(m)
+// 要转换的类型的数据放在类型(数据)
+let tempM = Double(m)
+let result = n + tempM
+
+// 将浮点型转为整型 Int(n) -> 强制转换不会进行四舍五入
+let tempN = Int(n)
+let results = m + tempN
+
+
+
+
+
+
